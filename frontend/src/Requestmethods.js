97d8b6307378ce68d6539115c259372a58f6050c
Yashasvi <98901285+Yashasvi-VII@users.noreply.github.com>
2022-04-06 20:55:39 +0530

    Update Requestmethods.js

--- frontend/src/Requestmethods.js
+++ frontend/src/Requestmethods.js
@@ -1,9 +1,7 @@
 import axios from "axios";
-const BASEURL = "http://localhost:4000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZmViMWJlYzkwNjA4ZWJkYjllZjFiZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NDYwMDkyOCwiZXhwIjoxNjQ0ODYwMTI4fQ.wgir-1FY2veuKZSJ9t27cuAKf_sExJxs3FR9oZteiMQ";
-// JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//   .accessToken;
+const BASEURL = Backend Baseurl 
+const TOKEN =provide token;
+
 export const publicRequest = axios.create({
   baseURL: BASEURL,
 });
